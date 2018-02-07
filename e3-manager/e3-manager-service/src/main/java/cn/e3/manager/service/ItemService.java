930f3b4a59da941dc380cb6e915fef933b7524ac
wanghuining <1402601964@qq.com>
2018-02-08 00:08:28 +0800

    manager

---
+++ e3-manager/e3-manager-service/src/main/java/cn/e3/manager/service/ItemService.java
@@ -0,0 +1,12 @@
+package cn.e3.manager.service;
+
+import cn.e3.pojo.TbItem;
+public interface ItemService {
+	/*
+	 * 商品需求:根据商品id查询商品数据
+	 * 参数:Long itemId
+	 * 返回值 TbItem
+	 */
+	public TbItem findItemById(Long itemId);
+
+}
